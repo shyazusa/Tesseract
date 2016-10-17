8290f877ec6492cdf2b19b16f1a8d68d7268e02c
shy_azusa <shy.azusa@gmail.com>
2016-10-17 13:30:26 +0900

    Fix https

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ window.addEventListener('load', function() {
   document.getElementById('submit').addEventListener('click', function() {
     const image = document.getElementById('url').value;
     document.getElementById('result').innerHTML = '<div id="img"><img src="' + image + '"/></div><div id="tesseract"></div>';
-    document.getElementById('tesseract').innerHTML = '読み込み中です…….';
-    Tesseract.recognize(image, {lang: 'jpn'}).then(function(result) {
-      document.getElementById('tesseract').innerHTML = result.html;
-    });
+    if (image.match(/^https:\/\/$/)) {
+      document.getElementById('tesseract').innerHTML = '読み込み中です…….';
+      Tesseract.recognize(image, {lang: 'jpn'}).then(function(result) {
+        document.getElementById('tesseract').innerHTML = result.html;
+      });
+    } else {
+      document.getElementById('tesseract').innerHTML = 'URLがhttps://ではないようです．<br />\nhttps://から始まるURLをお願いします．';
+    }
   });
 });
